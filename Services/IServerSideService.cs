ba55c8ba9818458e0bc366ec06da733a63a79a2f
Khurram Shahzad <KhurramShahzad@tfl.gov.uk>
2019-02-09 15:54:07 +0000

    add an interface for the service for ioc

---
+++ Services/IServerSideService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Server.Side.Events.Models;
+
+namespace Server.Side.Events.Services
+{
+    public interface IServerSideService
+    {
+        Guid Add(ServerSideClient client);
+        Task NotifyAllClients(string message);
+        void RemoveClient(Guid clientId);
+    }
+}
